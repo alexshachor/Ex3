b107ed9663e3255bd347f7b5acf499fe8ced9d85
avraham miletzky <avraham.miletzky@live.biu.ac.il>
2019-05-30 20:57:06 +0300

    some changes.

--- Ex3/Models/FileManager.cs
+++ Ex3/Models/FileManager.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace Ex3.Models
 {
-    public class FileManager<T>
+    public class FileManager<T> where T : ISerializable
     {
         public bool SaveData(string fileName, T dataCollection)
         {
             bool hasDataSaved = false;
-
             //TODO: serialize the list, so it will be saved in the file.
 
             return hasDataSaved;
